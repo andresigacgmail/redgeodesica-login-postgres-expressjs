0c42639b342405bbdbbdc031c1f42c31590cf89e
homeristico <homeristico@hotmail.com>
2023-04-11 16:21:39 -0500

    crud usuario completo

--- controlador/UsuarioController.js
+++ controlador/UsuarioController.js
@@ -10,6 +10,18 @@ const registrarUsuario = async (req, res) => {
     res.status(201).json(await usuarioService.guardarUsuarios(req.body));
 }
 
+const obtenerUsuario = async (req, res) => {
+    res.status(200).json(await usuarioService.obtenerUsuario(req.params.id));
+}
+
+const actualizarUsuario = async (req, res) => {
+    res.status(201).json(await usuarioService.actualizarUsuario(req.body, req.params.id));
+}
+
+const eliminarUsuario = async (req, res) => {
+    res.status(204).json(await usuarioService.eliminarUsuario(req.params.id));
+}
+
 module.exports = {
-    listaUsuarios, registrarUsuario
+    listaUsuarios, registrarUsuario, obtenerUsuario, actualizarUsuario, eliminarUsuario
 }
--- routes/UsuarioRoutes.js
+++ routes/UsuarioRoutes.js
@@ -4,6 +4,9 @@ const usuarioController = require('./../controlador/UsuarioController');
 
 router.get('/usuario', usuarioController.listaUsuarios);
 router.post('/usuario', usuarioController.registrarUsuario);
+router.get('/usuario/:id', usuarioController.obtenerUsuario);
+router.put('/usuario/:id', usuarioController.actualizarUsuario);
+router.delete('/usuario/:id', usuarioController.eliminarUsuario);
 
 
 
