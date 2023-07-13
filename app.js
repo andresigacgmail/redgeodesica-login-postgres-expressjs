82255b49f114c1e69fe7fe22c35620cae83d4745
homeristico <homeristico@hotmail.com>
2023-07-13 18:36:54 -0500

    changed conexion

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
 const app = express();
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/auth.homeristicodeath.tech/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/auth.homeristicodeath.tech/fullchain.pem')
+};
+
 const cors = require('cors');
 
 app.use(cors({origin:'*'}));
@@ -17,6 +25,10 @@ app.use('/api', usuarioRoutes);
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log('listen...')
+// app.listen(PORT, () => {
+//     console.log('listen...')
+// });
+
+https.createServer(options, app).listen(3001, function () {
+    console.log('Servidor HTTPS escuchando en el puerto 3001');
 });
