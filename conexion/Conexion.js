452b34c57a6204618fc2ca0782dbd638341f8724
homeristico <homeristico@hotmail.com>
2023-10-08 01:47:38 -0500

    conex up

--- conexion/Conexion.js
+++ conexion/Conexion.js
@@ -1,6 +1,6 @@
 
 var pgp = require("pg-promise")(/*options*/);
-var db = pgp("postgres://postgres:1234@localhost:5432/redgeodesica");
+var db = pgp("postgres://postgres:1234@10.0.0.225:5432/redgeodesica");
 
 // var pgp = require("pg-promise")(/*options*/);
 // var db = pgp("postgres://gis_owner_centroctrl_prod:9CU8uZSuzBDpTssR@PGSQLPRD01AZ/egdb_cartografia");
