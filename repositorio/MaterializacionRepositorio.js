58a173959d7bd54a3cb7e691c1ccebdecf3ddec1
homeristico <homeristico@hotmail.com>
2023-10-12 16:27:06 -0500

    agregada vereda a la consulta

--- repositorio/MaterializacionRepositorio.js
+++ repositorio/MaterializacionRepositorio.js
@@ -3,7 +3,9 @@ const db = require('./../conexion/Conexion');
 const accesoGeneralPorEstacion = async (estacion) => {    
     let intEstacon = parseInt(estacion);
     try {
-        return await db.one(`select * from materializacion where estacion = $1`,[intEstacon]);
+        return await db.one(`select me.t_id as estacion_id, identificador, ma.acceso_general, ma.descripción_detallada, 
+                            ve.nombre as vereda from main_estacion me left join materializacion ma on me.t_id = ma.estacion 
+                            left join veredas ve on me.vereda = ve.codigo where me.t_id = $1`,[intEstacon]);
     } catch (error) {
         console.log(error);
         return null;
