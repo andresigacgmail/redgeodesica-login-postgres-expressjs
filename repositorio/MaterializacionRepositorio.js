457c74c39b8a607a7bb321d569c5568095cd6e32
homeristico <homeristico@hotmail.com>
2023-11-17 15:08:23 -0500

    agregados campos a la consulta materializacion

--- repositorio/MaterializacionRepositorio.js
+++ repositorio/MaterializacionRepositorio.js
@@ -4,8 +4,10 @@ const accesoGeneralPorEstacion = async (estacion) => {
     let intEstacon = parseInt(estacion);
     try {
         return await db.one(`select me.t_id as estacion_id, identificador, ma.acceso_general, ma.descripción_detallada, 
-                            ve.nombre as vereda, ma.observacion from main_estacion me left join materializacion ma on me.t_id = ma.estacion 
-                            left join veredas ve on me.vereda = ve.codigo where me.t_id = $1`,[intEstacon]);
+                             ve.nombre as vereda, ma.observacion, ma.fecha_descripcion, ma.monumentado_por_1, 
+                             ma.ancho_mts, ma.largo_mts, ma.altura_mts, ma.actualizó 
+                             from main_estacion me left join materializacion ma on me.t_id = ma.estacion 
+                             left join veredas ve on me.vereda = ve.codigo where me.t_id = $1`,[intEstacon]);
     } catch (error) {
         console.log(error);
         return null;
