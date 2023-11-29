70f254e634cb75e03166ef7a662c0a615bc083a8
homeristico <homeristico@hotmail.com>
2023-11-29 17:38:19 -0500

    new branch

--- conexion/Conexion.js
+++ conexion/Conexion.js
@@ -2,8 +2,8 @@
 // var pgp = require("pg-promise")(/*options*/);
 // var db = pgp("postgres://postgres:1234@localhost:5432/redgeodesica");
 
-// var pgp = require("pg-promise")(/*options*/);
-// var db = pgp("postgres://gis_owner_centroctrl_prod:9CU8uZSuzBDpTssR@PGSQLPRD01AZ/egdb_cartografia");
+var pgp = require("pg-promise")(/*options*/);
+var db = pgp("postgres://gis_owner_centroctrl_prod:9CU8uZSuzBDpTssR@PGSQLPRD01AZ/egdb_cartografia");
 
 // var pgp = require("pg-promise")(/*options*/);
 // var db = pgp("postgres://gis_owner_centroctrl_prod:9CU8uZSuzBDpTssR@10.23.11.24:5432/egdb_cartografia");
@@ -11,17 +11,17 @@
 // var pgp = require("pg-promise")(/*options*/);
 // var db = pgp("postgres://gis_owner_centroctrl_prod:9CU8uZSuzBDpTssR@PGSQLPRD01AZ/egdb_cartografia");
 
-var pgp = require("pg-promise")();
-const db = pgp({
-    user: 'fl0user',
-    password: '1AMeWxsLzCm2',
-    host: 'ep-weathered-rain-22745194.us-east-2.aws.neon.fl0.io',
-    port: '5432',
-    database: 'redgeodesica',
-    ssl: {
-      // Habilita la conexión SSL/TLS segura
-      rejectUnauthorized: false, // Esto permite conexiones SSL no autenticadas, ten cuidado en producción
-    },
-  });
+// var pgp = require("pg-promise")();
+// const db = pgp({
+//     user: 'fl0user',
+//     password: '1AMeWxsLzCm2',
+//     host: 'ep-weathered-rain-22745194.us-east-2.aws.neon.fl0.io',
+//     port: '5432',
+//     database: 'redgeodesica',
+//     ssl: {
+//       // Habilita la conexión SSL/TLS segura
+//       rejectUnauthorized: false, // Esto permite conexiones SSL no autenticadas, ten cuidado en producción
+//     },
+//   });
 
 module.exports = db;
